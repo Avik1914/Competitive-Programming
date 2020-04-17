c37ea729db10214f0b068deda06606975f731ab4
Avik <62366747+Warrior1414@users.noreply.github.com>
2020-04-17 00:54:36 -0400

    Create Next Greater Element II.java

---
+++ Leetcode/Next Greater Element II.java
@@ -0,0 +1,28 @@
+/*
+Given a circular array (the next element of the last element is the first element of the array), print the Next Greater Number for every element. The Next Greater Number of a number x is the first greater number to its traversing-order next in the array, which means you could search circularly to find its next greater number. If it doesn't exist, output -1 for this number.
+
+Example 1:
+Input: [1,2,1]
+Output: [2,-1,2]
+Explanation: The first 1's next greater number is 2; 
+The number 2 can't find next greater number; 
+The second 1's next greater number needs to search circularly, which is also 2.
+Note: The length of given array won't exceed 10000.
+*/
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        Stack<Integer> stack=new Stack();
+        int len=nums.length;
+        int[] res=new int[len];
+        Arrays.fill(res,-1);
+        
+        for(int i=0;i<2*len;i++){
+            int val=i%len;
+            while(!stack.isEmpty() && nums[stack.peek()]<nums[val])
+                res[stack.pop()]=nums[val];
+            stack.push(val);
+        }
+        return res;
+    }
+}
