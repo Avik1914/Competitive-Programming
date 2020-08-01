4cd61c74a9f23ad473a67d235a37534c0074b37c
Avik <62366747+Avik1914@users.noreply.github.com>
2020-08-01 09:14:29 -0400

    Create Solution.java

---
+++ Leetcode/August Challenge/Detect Capital/Solution.java
@@ -0,0 +1,41 @@
+/*
+Given a word, you need to judge whether the usage of capitals in it is right or not.
+
+We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Otherwise, we define that this word doesn't use capitals in a right way.
+ 
+
+Example 1:
+
+Input: "USA"
+Output: True
+ 
+
+Example 2:
+
+Input: "FlaG"
+Output: False
+ 
+
+Note: The input will be a non-empty word consisting of uppercase and lowercase latin letters.
+*/
+
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        char[] arr=word.toCharArray();
+        int len=arr.length;
+        int cnt=0;
+        
+        for(int i=0;i<len;i++){
+            if(Character.isUpperCase(arr[i]))
+                cnt++;
+        }
+        if(cnt==1)
+            return Character.isUpperCase(arr[0]);
+        return cnt==0 || cnt==len;
+    }
+}
