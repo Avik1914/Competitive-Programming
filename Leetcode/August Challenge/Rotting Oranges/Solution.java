168ff56d5105fae852298068059cdfc07ccd42bd
Avik <62366747+Avik1914@users.noreply.github.com>
2020-08-09 10:43:21 -0400

    Create Solution.java

---
+++ Leetcode/August Challenge/Rotting Oranges/Solution.java
@@ -0,0 +1,81 @@
+/*
+In a given grid, each cell can have one of three values:
+
+the value 0 representing an empty cell;
+the value 1 representing a fresh orange;
+the value 2 representing a rotten orange.
+Every minute, any fresh orange that is adjacent (4-directionally) to a rotten orange becomes rotten.
+
+Return the minimum number of minutes that must elapse until no cell has a fresh orange.  If this is impossible, return -1 instead.
+
+ 
+
+Example 1:
+
+
+
+Input: [[2,1,1],[1,1,0],[0,1,1]]
+Output: 4
+Example 2:
+
+Input: [[2,1,1],[0,1,1],[1,0,1]]
+Output: -1
+Explanation:  The orange in the bottom left corner (row 2, column 0) is never rotten, because rotting only happens 4-directionally.
+Example 3:
+
+Input: [[0,2]]
+Output: 0
+Explanation:  Since there are already no fresh oranges at minute 0, the answer is just 0.
+ 
+
+Note:
+
+1 <= grid.length <= 10
+1 <= grid[0].length <= 10
+grid[i][j] is only 0, 1, or 2.
+*/
+
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int[][] dir={{-1,0},{0,-1},{1,0},{0,1}};
+        int m=grid.length;
+        int n=grid[0].length;
+        int noOfFreshOrange=0;
+        Queue<Integer> queue=new LinkedList();
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1)
+                    noOfFreshOrange++;
+                else if(grid[i][j]==2)
+                    queue.add(n*i+j);
+                    
+            }
+        }
+        if(noOfFreshOrange==0)
+            return 0;
+        int res=0;
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            //System.out.println(queue.toString());
+            for(int i=0;i<size;i++){
+                int val=queue.poll();
+                int r=val/n;
+                int c=val%n;
+                for(int[] d:dir){
+                    int nr=d[0]+r;
+                    int nc=d[1]+c;
+                    if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]==1){
+                        noOfFreshOrange--;
+                        grid[nr][nc]=2;
+                        queue.add(nr*n+nc);
+                    }
+                }
+            }
+            res++;
+        }
+       return noOfFreshOrange==0?res-1:-1; 
+        
+        
+    }
+}
