d5e4b473e95d3cc36ebbc36011e6221b69d5c426
Avik <62366747+Avik1914@users.noreply.github.com>
2020-12-05 12:13:04 +0530

    working commit - Created using LeetHub

--- bank-account-summary-ii/bank-account-summary-ii.sql
+++ bank-account-summary-ii/bank-account-summary-ii.sql
@@ -1,3 +1,1 @@
-/* Write your PL/SQL query statement below */
-​
-select m.name as name,y.summ as balance from users m,(select u.account as account_number,sum(t.amount) as summ  from users u,transactions t where u.account=t.account group by u.account) y where m.account=y.account_number and y.summ>10000;
+select u.name as Name,sum(t.amount) as Balance from users u,transactions t where u.account=t.account group by u.name having sum(t.amount)>10000;
