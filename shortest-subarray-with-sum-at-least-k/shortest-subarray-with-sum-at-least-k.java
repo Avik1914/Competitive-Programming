911b53774e39f22ae79a455683153a0cf8eaeb71
Avik <62366747+Avik1914@users.noreply.github.com>
2021-01-05 23:54:18 +0530

    Time: 66 ms (9.35%), Space: 48.1 MB (32.63%) - LeetHub

--- shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.java
+++ shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.java
@@ -1,33 +1,20 @@
 class Solution {
     public int shortestSubarray(int[] A, int K) {
         int len=A.length;
-        int sum=A[0];
-        if(len==1 && sum>=K)
-            return 1;
-        Deque<int[]> deque=new LinkedList();
-        deque.add(new int[]{sum,0});
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[0]==b[0]?a[1]-b[1]:a[0]-b[0]);
+        int sum=0;
         int res=len+1;
-        for(int i=1;i<len;i++){
-            if(sum+A[i]<A[i]){
-                deque.clear();
-                deque.add(new int[]{A[i],i});
-                sum=A[i];
-            }
-            else{
-                sum=sum+A[i];
-                int ind=i;
-                while(!deque.isEmpty() && deque.getLast()[0]>=sum)
-                    ind=deque.removeLast()[1];
-                deque.addLast(new int[]{sum,ind});
-            }
-            
-            while(!deque.isEmpty() && sum-deque.getFirst()[0]>=K)
-                 deque.removeFirst();
-        
+        for(int i=0;i<len;i++){
+            sum+=A[i];
             if(sum>=K)
-                res=Math.min(i-deque.getFirst()[1]+1,res);
+                res=Math.min(res,i+1);
             
+            while(!pq.isEmpty() && sum-pq.peek()[0]>=K)
+                res=Math.min(res,i-pq.poll()[1]);
+            
+            pq.add(new int[]{sum,i});
         }
+        
         return res==len+1?-1:res;
     }
 }
