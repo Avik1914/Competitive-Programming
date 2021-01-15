5c2fa6cc18e1c42c3ddea0d4c7e35547549cd74f
Avik <62366747+Avik1914@users.noreply.github.com>
2021-01-15 12:09:44 +0530

    Create Solution.java

---
+++ Leetcode/January Challenge/Minimum Operations to Reduce X to Zero/Solution.java
@@ -0,0 +1,54 @@
+/*
+You are given an integer array nums and an integer x. In one operation, you can either remove the leftmost or the rightmost element from the array nums and subtract its value from x. Note that this modifies the array for future operations.
+
+Return the minimum number of operations to reduce x to exactly 0 if it's possible, otherwise, return -1.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,4,2,3], x = 5
+Output: 2
+Explanation: The optimal solution is to remove the last two elements to reduce x to zero.
+Example 2:
+
+Input: nums = [5,6,7,8,9], x = 4
+Output: -1
+Example 3:
+
+Input: nums = [3,2,20,1,1,3], x = 10
+Output: 5
+Explanation: The optimal solution is to remove the last three elements and the first two elements (5 operations in total) to reduce x to zero.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 104
+1 <= x <= 109
+*/
+
+class Solution {
+    public int minOperations(int[] nums, int x) {
+        int len=nums.length;
+        int sum=0;
+        for(int n:nums)
+            sum+=n;
+        
+        Map<Integer,Integer> map=new HashMap();
+        map.put(0,-1);
+        sum=sum-x;
+        int val=0;
+        int res=len+1;
+        if(sum==0)
+            return len;
+        for(int i=0;i<len;i++){
+            val+=nums[i];
+            if(map.containsKey(val-sum))
+                res=Math.min(map.get(val-sum)+len-i,res);
+            
+            map.put(val,i);
+        }
+        return res==len+1?-1:res;
+    }
+}
