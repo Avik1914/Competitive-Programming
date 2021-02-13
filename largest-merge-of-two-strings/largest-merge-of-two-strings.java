a63cf7010eb7515b985fd0091bb7b6d740e044e3
Avik <62366747+Avik1914@users.noreply.github.com>
2021-02-13 07:04:37 -0500

    Time: 76 ms (56.82%), Space: 42 MB (47.04%) - LeetHub

---
+++ largest-merge-of-two-strings/largest-merge-of-two-strings.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String largestMerge(String word1, String word2) {
+        if(word1.length()==0)
+            return word2;
+        if(word2.length()==0)
+            return word1;
+        
+        if(word1.compareTo(word2)>0)
+            return ""+word1.charAt(0)+largestMerge(word1.substring(1),word2);
+        return ""+word2.charAt(0)+largestMerge(word1,word2.substring(1));
+    }
+}
