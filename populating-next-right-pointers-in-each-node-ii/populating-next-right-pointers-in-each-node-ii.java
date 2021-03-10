1afc547c45eca9c27021f2874d706202875f3789
Avik <62366747+Avik1914@users.noreply.github.com>
2021-03-09 23:25:05 -0500

    Time: 0 ms (100.00%), Space: 39 MB (29.02%) - LeetHub

--- populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.java
+++ populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.java
@@ -1,47 +1,52 @@
 /*
 // Definition for a Node.
 class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-​
-    public Node() {}
-    
-    public Node(int _val) {
-        val = _val;
-    }
-​
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
 };
 */
-​
+
 class Solution {
-    public Node connect(Node root) {
-        dfs(root,null);
-        return root;
-    }
-    
-    public void dfs(Node root,Node prev){
-        if(root==null)
-            return;
-        if(prev!=null){
-            if(prev.left==root && prev.right!=null)
-                root.next=prev.right;
-            else if(prev.next!=null){
-                prev=prev.next;
-                while(prev!=null && prev.left==null && prev.right==null)
-                    prev=prev.next;
-                if(prev!=null)    
-                root.next=prev.left!=null?prev.left:prev.right;
-            }
-        }
-        dfs(root.right,root);
-        dfs(root.left,root);
-    }
+    public Node connect(Node root) {
+        Node cur=root,prev=null,head=null;
+        
+        while(cur!=null){
+            while(cur!=null){
+                if(cur.left!=null){
+                    if(prev!=null)
+                        prev.next=cur.left;
+                    else
+                        head=cur.left;
+                    prev=cur.left;
+                }
+                if(cur.right!=null){
+                    if(prev!=null)
+                        prev.next=cur.right;
+                    else
+                        head=cur.right;
+                    prev=cur.right;
+                }
+                cur=cur.next;
+            }
+            cur=head;
+            prev=null;
+            head=null;
+        }
+        return root;
+    }
 }
