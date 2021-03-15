114c2ceda841d420c3e4a9deaa05693a14c17dde
Avik <62366747+Avik1914@users.noreply.github.com>
2021-03-14 20:42:25 -0400

    Time: 46 ms (12.37%), Space: 40 MB (20.92%) - LeetHub

--- coin-change/coin-change.java
+++ coin-change/coin-change.java
@@ -1,22 +1,24 @@
 class Solution {
-    
+    Integer[][] dp;
     public int coinChange(int[] coins, int amount) {
-        int len=coins.length;
-        int[] dp=new int[amount+1];
+        dp=new Integer[coins.length][amount+1];
+        int a=dfs(coins,0,amount);
+        return a==Integer.MAX_VALUE?-1:a;
+    }
+    
+    public int dfs(int[] coins,int start,int amount){
+        if(amount==0)
+            return 0;
+        if(start>=coins.length || amount<0)
+            return Integer.MAX_VALUE;
+        if(dp[start][amount]==null){
         int res=0;
-        Arrays.fill(dp,Integer.MAX_VALUE);
-        Arrays.sort(coins);
-        dp[0]=0;
-        
-        for(int i=1;i<=amount;i++){
-            for(int j=0;j<len;j++){
-               if(i>=coins[j]){
-                   if(dp[i-coins[j]]!=Integer.MAX_VALUE)
-                        dp[i]=Math.min(dp[i],1+dp[i-coins[j]]);      
-               }
-            }
+        int a=dfs(coins,start,amount-coins[start]);
+        int b=dfs(coins,start+1,amount);
+        if(a!=Integer.MAX_VALUE)
+            a++;
+        dp[start][amount]=Math.min(a,b);
         }
-        
-        return dp[amount]==Integer.MAX_VALUE?-1:dp[amount];
+        return dp[start][amount];
     }
 }
