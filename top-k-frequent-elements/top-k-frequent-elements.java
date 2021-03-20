a1a75305b874204a489ca49b97d1abcbb8977aff
Avik <62366747+Avik1914@users.noreply.github.com>
2021-03-19 21:50:39 -0400

    Time: 16 ms (20.01%), Space: 46.1 MB (18.17%) - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.java
+++ top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,20 +1,31 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        int len=nums.length;
+        
+        Set<Integer>[] bucket=new HashSet[len+1];
         Map<Integer,Integer> map=new HashMap();
-        for(int n:nums)
-            map.put(n,map.getOrDefault(n,0)+1);
-        
-        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->map.get(a)-map.get(b));
-        
-        for(int val:map.keySet()){
-            pq.add(val);
-            if(pq.size()>k)
-                pq.poll();
+        bucket[0]=new HashSet();
+        for(int i=0;i<len;i++){
+            bucket[i+1]=new HashSet();
+            int val=map.getOrDefault(nums[i],0);
+            bucket[val].remove(new Integer(nums[i]));
+            bucket[val+1].add(nums[i]);
+            map.put(nums[i],val+1);
         }
-        int[] res=new int[k];
-        for(int i=0;i<k;i++)
-            res[i]=pq.poll();
+        int itr=len;
         
+        int[] res=new int[k];
+        while(itr>=0 && k>0){
+            if(!bucket[itr].isEmpty()){
+                for(int val:bucket[itr]){
+                    res[--k]=val;
+                    if(k==0)
+                        return res;
+                }
+            }
+            itr--;
+        }
         return res;
+        
     }
 }
