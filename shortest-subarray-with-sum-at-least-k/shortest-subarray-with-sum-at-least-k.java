f77d4b8bfa46f0cefa80f94f61f529b5ed34c14b
Avik <62366747+Avik1914@users.noreply.github.com>
2021-03-27 21:21:38 -0400

    Time: 68 ms (8.77%), Space: 48.1 MB (35.42%) - LeetHub

--- shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.java
+++ shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.java
@@ -1,20 +1,27 @@
 class Solution {
-    public int shortestSubarray(int[] A, int K) {
-        int len=A.length;
-        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[0]==b[0]?a[1]-b[1]:a[0]-b[0]);
-        int sum=0;
-        int res=len+1;
-        for(int i=0;i<len;i++){
-            sum+=A[i];
-            if(sum>=K)
-                res=Math.min(res,i+1);
-            
-            while(!pq.isEmpty() && sum-pq.peek()[0]>=K)
-                res=Math.min(res,i-pq.poll()[1]);
-            
-            pq.add(new int[]{sum,i});
-        }
-        
-        return res==len+1?-1:res;
-    }
+    public int shortestSubarray(int[] A, int K) {
+        PriorityQueue<int[]> pq=new PriorityQueue<>
+                ((a,b)->a[0]==b[0]?b[1]-a[1]:a[0]-b[0]);
+        int len=A.length;
+        int sum=0;
+        int start=-1;
+        int val=0;
+        int res=len+1;
+        for(int i=0;i<len;i++){
+            sum+=A[i];
+            val=0;
+            while(!pq.isEmpty() && sum-pq.peek()[0]>=K){
+                int[] pop=pq.poll();
+                if(start<pop[1]){
+                    start=pop[1];
+                    val=pop[0];
+                }
+            }
+            if(sum-val>=K)
+                res=Math.min(res,i-start);
+            pq.add(new int[]{sum,i});
+        }
+        
+        return res==len+1?-1:res;
+    }
 }
