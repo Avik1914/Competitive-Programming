64351c63471987ee00d8af95664a313f7e159ee2
Avik <62366747+Avik1914@users.noreply.github.com>
2021-03-27 21:48:17 -0400

    Time: 41 ms (16.17%), Space: 103.6 MB (5.01%) - LeetHub

--- shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.java
+++ shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.java
@@ -1,25 +1,24 @@
 class Solution {
     public int shortestSubarray(int[] A, int K) {
-        PriorityQueue<int[]> pq=new PriorityQueue<>
-                ((a,b)->a[0]==b[0]?b[1]-a[1]:a[0]-b[0]);
+        Deque<int[]> deque=new LinkedList();
+        
         int len=A.length;
         int sum=0;
-        int start=-1;
-        int val=0;
         int res=len+1;
+        int start=0;
         for(int i=0;i<len;i++){
             sum+=A[i];
-            val=0;
-            while(!pq.isEmpty() && sum-pq.peek()[0]>=K){
-                int[] pop=pq.poll();
-                if(start<pop[1]){
-                    start=pop[1];
-                    val=pop[0];
-                }
-            }
-            if(sum-val>=K)
-                res=Math.min(res,i-start);
-            pq.add(new int[]{sum,i});
+            start=i;
+            
+            int val=0;
+            while(!deque.isEmpty() && sum-deque.peekFirst()[0]>=K)
+                res=Math.min(res,i-deque.pollFirst()[1]);
+            while(!deque.isEmpty() && deque.peekLast()[0]>=sum)
+                start=deque.pollLast()[1];
+            
+            if(sum>=K)
+                res=Math.min(res,i+1);
+            deque.add(new int[]{sum,i});
         }
         
         return res==len+1?-1:res;
