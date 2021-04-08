b5887113c9c2e79f2f5577602c19ed923b93c6b7
Avik <62366747+Avik1914@users.noreply.github.com>
2021-04-07 23:57:03 -0400

    Time: 0 ms (100.00%), Space: 6 MB (38.71%) - LeetHub

---
+++ truncate-sentence/truncate-sentence.c
@@ -0,0 +1,10 @@
+char * truncateSentence(char * s, const int k){
+        int found = 0;
+        char *pc = s;
+        while (found < k && *pc != '\0') {
+                ++pc;
+                found += (*pc == ' ');
+        }
+        *pc = '\0';
+        return s;
+}
