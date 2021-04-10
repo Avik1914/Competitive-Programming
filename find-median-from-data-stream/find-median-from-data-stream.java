590611590e1d10160a17e65da15bae2350d0c010
Avik <62366747+Avik1914@users.noreply.github.com>
2021-04-10 14:10:34 -0400

    Time: 53 ms (44.36%), Space: 50 MB (92.28%) - LeetHub

--- find-median-from-data-stream/find-median-from-data-stream.java
+++ find-median-from-data-stream/find-median-from-data-stream.java
@@ -12,17 +12,11 @@ class MedianFinder {
        int maxSize=pq1.size();
        int minSize=pq2.size();
        
-        if(maxSize>0 && pq1.peek()>=num){
-            pq1.add(num);
-            maxSize++;
-        }else{
-            pq2.add(num);
-            minSize++;
-        }
-        if(maxSize-minSize>1)
-            pq2.add(pq1.poll());
-        if(minSize-maxSize>0)
-            pq1.add(pq2.poll());
+       pq1.add(num);
+       pq2.add(pq1.poll());
+       if(pq2.size()>pq1.size())
+           pq1.add(pq2.poll());
+       
     }
     
     public double findMedian() {
@@ -30,10 +24,8 @@ class MedianFinder {
         int size2=pq2.size();
         if(size1==size2)
             return (double)(pq1.peek()+pq2.peek())/2.0;
-        else if(size1>0)
-            return (double)pq1.peek();
-        else
-            return (double)pq2.peek();
+        
+        return (double)pq1.peek();
        
     }
 }
