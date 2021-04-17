73c1d075d07337ba1b1f49fdfdf25e8f05692636
Avik <62366747+Avik1914@users.noreply.github.com>
2021-04-17 12:21:16 -0400

    Time: 4 ms (63.67%), Space: 40.5 MB (83.62%) - LeetHub

--- find-k-closest-elements/find-k-closest-elements.java
+++ find-k-closest-elements/find-k-closest-elements.java
@@ -1,39 +1,18 @@
 class Solution {
-    public List<Integer> findClosestElements(int[] nums, int k, int x) {
-        int len=nums.length;
-        
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int len=arr.length;
         int lo=0;
-        int hi=len-1;
-        int min=Integer.MAX_VALUE;
-        int ind=-1;
-        while(lo<=hi){
+        int hi=len-k;
+        
+        while(lo<hi){
             int mid=(lo+hi)/2;
-            if(min>Math.abs(x-nums[mid])){
-                min=Math.abs(x-nums[mid]);
-                ind=mid;
-            }
-            if(nums[mid]<x)
+            if(x-arr[mid]>arr[mid+k]-x)
                 lo=mid+1;
             else
-                hi=mid-1;
+                hi=mid;
+                
         }
         
-        int r=ind,l=ind;
-        
-        while((r<len || l>=0) && k-->=0){
-            if(r==len)
-                l--;
-            else if(l<0)
-                r++;
-            else if(Math.abs(nums[r]-x)<Math.abs(nums[l]-x))
-                r++;
-            else
-                l--;
-        }
-        List<Integer> res=new ArrayList();
-        for(int i=l+1;i<r;i++)
-            res.add(nums[i]);
-        
-        return res;
+        return Arrays.stream(arr, lo, lo + k).boxed().collect(Collectors.toList());
     }
 }
