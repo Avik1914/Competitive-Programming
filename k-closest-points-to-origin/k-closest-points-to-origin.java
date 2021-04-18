022009e823866bbd4b0caa998ddbeb6a6b15ee80
Avik <62366747+Avik1914@users.noreply.github.com>
2021-04-17 20:39:00 -0400

    Time: 4 ms (92.22%), Space: 47.2 MB (79.72%) - LeetHub

--- k-closest-points-to-origin/k-closest-points-to-origin.java
+++ k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,19 +1,42 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        int len=points.length;
-        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
-                    
-        
-        for(int i=0;i<points.length;i++){
-            pq.add(points[i]);
-            if(pq.size()>k)
-                pq.poll();
+        int lo=0;
+        int hi=points.length-1;
+        while(lo<hi){
+            int val=quickSelect(points,lo,hi);
+            if(val==k)
+                break;
+            if(val<k)
+                lo=val+1;
+            else
+                hi=val-1;
         }
         int[][] res=new int[k][2];
-        
         for(int i=0;i<k;i++)
-            res[i]=pq.poll();
-        
+            res[i]=points[i];
         return res;
+    }
+    
+    
+    public int quickSelect(int[][] points,int lo,int hi){
+        int i=lo-1;
+      
+        int dist=points[hi][0]*points[hi][0]+points[hi][1]*points[hi][1];
+        
+        for(int k=lo;k<hi;k++){
+            int presDist=(points[k][0]*points[k][0])+(points[k][1]*points[k][1]);
+            if(presDist<dist){
+                swap(points,i+1,k);
+                i++;
+            }
+        }
+        swap(points,i+1,hi);
+        return i+1;
+    }
+    
+    public void swap(int[][] points,int i,int j){
+        int[] temp=points[i];
+        points[i]=points[j];
+        points[j]=temp;
     }
 }
