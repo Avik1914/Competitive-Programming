e29c2891b4d135e1484ef3a897c9604af92008b0
Avik <62366747+Avik1914@users.noreply.github.com>
2021-04-29 08:54:45 -0400

    Time: 9 ms (65.93%), Space: 38.8 MB (56.74%) - LeetHub

--- integer-to-english-words/integer-to-english-words.java
+++ integer-to-english-words/integer-to-english-words.java
@@ -3,7 +3,8 @@ class Solution {
     
 private final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
     
-private final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
+//private final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
+    
     
     public String numberToWords(int num) {
         if(num==0)
@@ -12,12 +13,17 @@ private final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
     }
     
     public String helper(int num){
-        if(num<20)
-            return LESS_THAN_20[num];
-        if(num<100)  return  (TENS[num/10]+" "+helper(num%10)).trim();
-        if(num<1000) return  (LESS_THAN_20[num/100]+" Hundred "+helper(num%100)).trim();
-        if(num<1000000) return (helper(num/1000)+" Thousand "+helper(num%1000)).trim();
-        if(num<1000000000) return (helper(num/1000000)+" Million "+helper(num%1000000)).trim();
+       if(num<20)
+           return LESS_THAN_20[num];
+       if(num<100)
+           return (TENS[num/10]+" "+helper(num%10)).trim();
+       if(num<1000)
+           return (helper(num/100)+" Hundred "+helper(num%100)).trim();
+        if(num<1000000)
+            return (helper(num/1000)+" Thousand "+helper(num%1000)).trim();
+        if(num<1000000000)
+            return (helper(num/1000000)+" Million "+helper(num%1000000)).trim();
         return (helper(num/1000000000)+" Billion "+helper(num%1000000000)).trim();
+        
     }
 }
