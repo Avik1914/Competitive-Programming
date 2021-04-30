54aea82c4291ee786ca9b90a61aa6ba7c6c299c1
Avik <62366747+Avik1914@users.noreply.github.com>
2021-04-30 01:02:54 -0400

    Time: 7 ms (86.49%), Space: 39.2 MB (21.79%) - LeetHub

--- maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,35 +1,33 @@
 class Solution {
     public int maxLength(List<String> arr) {
-        List<Integer> dp=new ArrayList();
-        dp.add(0);
-        int res=0;
-        int len=arr.size();
+        List<Integer> list=new ArrayList<>();
         
-        for(int i=0;i<len;i++){
-            char[] cArr=arr.get(i).toCharArray();
-            int val=0;
+       int res=0;
+        list.add(0);
+        
+        for(String s:arr){
+            int dp=0;
             boolean flag=true;
-            for(char c:cArr){
-                if((val & (1<<(c-'a')))!=0){
+            for(char c:s.toCharArray()){
+                int val=1<<(c-'a');
+                if((dp & val)!=0){
                     flag=false;
                     break;
                 }
-                val|=(1<<(c-'a'));
+                dp|=val;
             }
             if(!flag)
                 continue;
-            int size=dp.size();
-            for(int j=0;j<size;j++){
-                if((val & dp.get(j))==0){
-                    int p=(val|dp.get(j));
-                    dp.add(p);
-                    res=Math.max(res,Integer.bitCount(p));
+            int size=list.size();
+            for(int i=0;i<size;i++){
+                if((list.get(i) & dp)==0){
+                    int val=(list.get(i) | dp);
+                    list.add(val);
+                    res=Math.max(res,Integer.bitCount(val));
                 }
             }
-            
-            
         }
+        
         return res;
     }
-    
 }
