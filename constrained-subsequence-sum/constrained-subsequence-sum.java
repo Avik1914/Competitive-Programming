48390628bf080270c3f475364d233790b4098bbb
Avik <62366747+Avik1914@users.noreply.github.com>
2021-05-08 19:24:14 -0400

    Time: 17 ms (73.60%), Space: 58.3 MB (7.30%) - LeetHub

--- constrained-subsequence-sum/constrained-subsequence-sum.java
+++ constrained-subsequence-sum/constrained-subsequence-sum.java
@@ -1,26 +1,45 @@
 class Solution {
-    public int constrainedSubsetSum(int[] nums, int k) {
-       int len=nums.length;
-        int[] dp=new int[len];
-        int res=nums[len-1];
-        Deque<Integer> deque = new LinkedList<Integer>(); 
-        deque.add(len-1);
-        dp[len-1]=res;
-        
-        for(int i=len-2;i>=0;i--){
-            if(!deque.isEmpty() && deque.getLast()-i>k)
-                deque.removeLast();
-            int val=deque.isEmpty()?0:dp[deque.getLast()];
-            dp[i]=Math.max(nums[i],nums[i]+val);
-            
-            
-            res=Math.max(res,dp[i]);
-            
-            while(!deque.isEmpty() && dp[deque.getFirst()]<dp[i])
-                deque.removeFirst();
-            deque.addFirst(i);
-        }
-        
-        return res;
-    }
+    
+    public int constrainedSubsetSum(int[] nums, int k) {
+        int len=nums.length;
+        int res=Integer.MIN_VALUE;
+        
+        
+        
+        
+        Deque<int[]> deque=new LinkedList<>();
+        int sum=0;
+        
+        for(int i=0;i<len;i++){
+            
+           if(!deque.isEmpty() && i-deque.peekFirst()[0]>k)
+              deque.pollFirst();
+            
+           sum=Math.max(nums[i],nums[i]+(deque.isEmpty()?0:deque.peekFirst()[1]));
+            
+           while(!deque.isEmpty() && sum>=deque.peekLast()[1])
+               deque.pollLast();
+            deque.add(new int[]{i,sum});
+            res=Math.max(res,sum);
+            
+        }
+        
+        
+        /*int[] dp=new int[len];
+        dp[len-1]=nums[len-1];
+        
+        
+        
+        for(int i=len-2;i>=0;i--){
+            dp[i]=nums[i];
+            int max=dp[i];
+            for(int j=i+1;j<Math.min(len,i+k+1);j++)
+                max=Math.max(max,dp[i]+dp[j]);
+            res=Math.max(max,res);
+            dp[i]=max;
+        }*/
+        
+        return res;
+    }
+    
 }
